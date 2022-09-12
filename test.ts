eb3cf5763ff2346e21c25a84cb592bbb40cbc92c
Nishanth <84061203+nishuhary@users.noreply.github.com>
2022-09-12 10:18:22 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,4 @@
-console.log(err)
+let resp = await fetch("https://google.com");
+console.log(resp.status); // 200
+console.log(resp.headers.get("Content-Type")); // "text/html"
+console.log(await resp.text()); // "Hello, World!"
