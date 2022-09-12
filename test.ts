7c039e60806d5f6d9fee3d8432edc353fcb187bd
Nishanth <84061203+nishuhary@users.noreply.github.com>
2022-09-12 10:21:19 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-let resp = await fetch("https://13.233.121.162:90");
+let resp = await fetch("http://13.233.121.162:90");
 console.log(resp.status); // 200
 console.log(resp.headers.get("Content-Type")); // "text/html"
 console.log(await resp.text()); // "Hello, World!"
