99ff111f61af821a452717f0714a6d08cf9d85eb
Nishanth <84061203+nishuhary@users.noreply.github.com>
2022-09-12 10:24:18 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -3,6 +3,10 @@ console.log(resp.status); // 200
 console.log(resp.headers.get("Content-Type")); // "text/html"
 console.log(await resp.text()); // "Hello, World!"
 
+
+const { userAgent } = navigator
+console.log(userAgent);
+
 if (userAgent.includes('Firefox/')) {
     console.log(userAgent)
 } else if (userAgent.includes('Edg/')) {
