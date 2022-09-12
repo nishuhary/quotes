cf2430210f822d7895e5e7199e507d1d01369722
Nishanth <84061203+nishuhary@users.noreply.github.com>
2022-09-12 10:23:16 +0530

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,3 +2,13 @@ let resp = await fetch("http://13.233.121.162:90");
 console.log(resp.status); // 200
 console.log(resp.headers.get("Content-Type")); // "text/html"
 console.log(await resp.text()); // "Hello, World!"
+
+if (userAgent.includes('Firefox/')) {
+    console.log(userAgent)
+} else if (userAgent.includes('Edg/')) {
+    console.log(userAgent)
+} else if (userAgent.includes('Chrome/')) {
+    console.log(userAgent)
+} else if (userAgent.includes('Safari/')) {
+    // Safari
+}
